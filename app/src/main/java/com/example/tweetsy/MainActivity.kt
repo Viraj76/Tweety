e64350f603268fcebafa41e8789bcb370e2bd3eb
Viraj76 <virajg.7680@gmail.com>
2023-08-25 23:19:40 +0530

    setting up app bar

--- app/src/main/java/com/example/tweetsy/MainActivity.kt
+++ app/src/main/java/com/example/tweetsy/MainActivity.kt
@@ -3,12 +3,19 @@ package com.example.tweetsy
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
@@ -27,12 +34,24 @@ import kotlinx.coroutines.launch
 class MainActivity : ComponentActivity() {
 
 
+    @OptIn(ExperimentalMaterial3Api::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         setContent {
             TweetsyTheme {
-                App()
+                Scaffold(
+                    topBar = {
+                        TopAppBar(
+                            title = { Text(text = "Tweetsy", color = Color.Blue)},
+                            modifier = Modifier.background(Color.Blue),
+                        )
+                    }
+                ) {
+                    Box (modifier = Modifier.padding(it)) {
+                        App()
+                    }
+                }
             }
         }
     }
